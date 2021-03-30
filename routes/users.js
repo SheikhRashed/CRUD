23438c035227436eed102fd63a4255672922cc4e
Sheikh Rashed <mdrashed1310@gmail.com>
2021-03-31 03:20:09 +0600

    Reinitalized project from gitHub

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 import express from 'express';
 
-import { getUsers, createUser, getUser, deleteUser, updateUser } from '../controllers/users.js';
+import {
+  getUsers,
+  createUser,
+  getUser,
+  deleteUser,
+  updateUser,
+} from '../controllers/users.js';
 
 const router = express.Router();
 
